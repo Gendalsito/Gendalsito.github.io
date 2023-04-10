8fc520584f5436f4e00641a53afed1d7e1729cf8
Gendalsito <gendalcod@gmail.com>
2023-04-10 16:28:28 -0400

    Primer commit subiendo la base de la página

---
+++ JavaScript/main.js
@@ -0,0 +1,83 @@
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+//show menu
+menuBtn.addEventListener('click', () => {
+
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display='inline-block'
+    document.querySelector(".style-switcher").classList.remove("open");//cerrar el menu de temas
+
+})
+// hide menu
+closeBtn.addEventListener('click', () => {
+
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display='none';
+
+})
+
+const navItems = menu.querySelectorAll('li');
+
+const changueActiveItem = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a');
+            link.classList.remove('active');
+        })
+}
+
+
+navItems.forEach(item => {
+
+    const link = item.querySelector('a');
+    link.addEventListener('click', () =>{
+        changueActiveItem();
+        link.classList.add('active');
+    })
+    
+});
+
+
+// add box shadow on scroll
+
+window.addEventListener('scroll',() => {
+    document.querySelector('nav').classList.toggle('show-box-shadow',window.scrollY > 0)
+})
+
+
+// CDNJS TYPING
+
+var typed = new Typed (".typing",{
+    strings:["UI/UX","UX","Front Development"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+
+//About section
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+
+    for(tablink of tablinks){
+
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents){
+
+        tabcontent.classList.remove("active-tab");
+
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
---
+++ JavaScript/style-switcher.js
@@ -0,0 +1,120 @@
+/* ======================= TOGGLE STYLE SWITCHER ============================== */
+
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener('click', () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+// hide style switcher on scroll
+window.addEventListener('scroll', () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+
+})
+
+// ======================== THEME COLORS =============================
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+
+        }
+        else {
+            style.setAttribute("disabled", "true");
+
+        }
+    })
+}
+// ======================== LENGUAGE TRANSLATION ==============================
+
+
+
+// ======================== THEME LIGHT DARK MODE =============================
+const dayNight = document.querySelector(".day-night");
+let dayON = false;
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  // si el usuario prefiere el modo oscuro, agregamos la clase "dark" al cuerpo
+  document.body.classList.add("dark");
+  dayON = true;
+}
+
+dayNight.addEventListener('click', () => {
+  document.body.classList.toggle("dark");
+  dayNight.querySelector("i").classList.toggle("uil-sunset");
+  dayNight.querySelector("i").classList.toggle("uil-moonset");
+  dayON = !dayON;
+
+  if (dayON) {
+    $(".ser-text").css("color", "black");
+   
+
+  } else {
+    $(".ser-text").css("color", "white");
+    
+  }
+});
+
+
+
+window.addEventListener('load', () => {
+
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("uil-sunset");
+
+
+    }
+
+    else {
+        dayNight.querySelector("i").classList.add("uil-moonset");
+
+    }
+
+})
+
+
+// Colores de las fuentes en los cuadros de habilidades
+
+
+
+    $(document).ready(function () {
+
+        $(".card").hover(function () {
+
+           
+            $(this).find(".ser-text").css("color", "white");
+
+            if (dayON) {
+
+                $(".card").mouseleave(function () {
+
+                    if (dayON) {
+
+                        $(this).find(".ser-text").css("color", "black");
+                    
+                    }
+                });
+
+            }
+            else {
+
+                $(this).find(".ser-text").css("color", "white");
+            
+
+            }
+
+
+        });
+
+
+    });
+
+
+
+
+
